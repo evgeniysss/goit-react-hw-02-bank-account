71ec7e94a8452cf06ced48d048f8acd1544218df
kievtechnoshop@gmail.com <swr83947>
2019-11-25 20:03:34 +0200

    hw-2-2

--- src/components/TransactionHistory/TransactionHistory.js
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -31,7 +31,7 @@ TransactionHistory.propTypes = {
     PropTypes.shape({
       id: PropTypes.string,
       type: PropTypes.string,
-      amount: PropTypes.string,
+      amount: PropTypes.number,
       date: PropTypes.string,
     }),
   ).isRequired,
